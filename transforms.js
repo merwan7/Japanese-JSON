ec990047bfafdebcf915aec7194c1ef9f4a398b0
Merwan Rodriguez <merwan@gmail.com>
2012-08-20 06:04:44 -0400

    added preliminary function names

--- transforms.js
+++ transforms.js
@@ -2,65 +2,69 @@
 // like this { Kana : Romaji } 
 // i.e. {"ん": "n", ... }
 
-var KatakanaToRomaji = {};
-var HiraganaToRomaji = {};
-var RomajiToHiragana = {};
-var RomajiToKatakana = {};
+function SimplestJSON(JapaneseJSON) {
+	var KatakanaToRomaji = {};
+	var HiraganaToRomaji = {};
+	var RomajiToHiragana = {};
+	var RomajiToKatakana = {};
 
-for (var parent in JSON) {
-	if (JSON.hasOwnProperty(parent)) {
-		var vowels = JSON[parent];
+	for (var parent in JapaneseJSON) {
+		if (JapaneseJSON.hasOwnProperty(parent)) {
+			var vowels = JapaneseJSON[parent];
 
-		for (var vowel in vowels) {
-			if (vowels.hasOwnProperty(vowel)) {
-				var types = vowels[vowel];
+			for (var vowel in vowels) {
+				if (vowels.hasOwnProperty(vowel)) {
+					var types = vowels[vowel];
 
-				for (var type in types) {
-					if (types.hasOwnProperty(type)) {
-						var character = types[type];
+					for (var type in types) {
+						if (types.hasOwnProperty(type)) {
+							var character = types[type];
 
-						HiraganaToRomaji[character["Hiragana"]] = character["Romaji"];
-						KatakanaToRomaji[character["Katakana"]] = character["Romaji"];
-						RomajiToHiragana[character["Romaji"]] 	= character["Hiragana"];
-						RomajiToKatakana[character["Romaji"]] 	= character["Katakana"];
+							HiraganaToRomaji[character["Hiragana"]] = character["Romaji"];
+							KatakanaToRomaji[character["Katakana"]] = character["Romaji"];
+							RomajiToHiragana[character["Romaji"]] 	= character["Hiragana"];
+							RomajiToKatakana[character["Romaji"]] 	= character["Katakana"];
+						}
 					}
 				}
 			}
 		}
 	}
 } 
+
 
 
 // Create something a bit more complicated, but simpler than the big guy
 // i.e. { k : [{ "カ" : "ka"}, ... ], ...}
+function SimpleJSON(JapaneseJSON) {
+	var RomajiToHiragana = {};
+	var RomajiToKatakana = {};
+	for (parent in JapaneseJSON) {
+		if (JapaneseJSON.hasOwnProperty(parent)) {
+			var vowels = JapaneseJSON[parent];
 
-var RomajiToHiragana = {};
-var RomajiToKatakana = {};
-for (parent in JSON) {
-	if (JSON.hasOwnProperty(parent)) {
-		var vowels = JSON[parent];
+			RomajiToHiragana[parent] = [];
+			RomajiToKatakana[parent] = [];
 
-		RomajiToHiragana[parent] = [];
-		RomajiToKatakana[parent] = [];
+			for (vowel in vowels) {
+				if (vowels.hasOwnProperty(vowel)) {
+					var types = vowels[vowel];
+					for (type in types) {
+						if (types.hasOwnProperty(type)) {
+							var character = types[type],
+								tmp1 = character["Romaji"],
+							 	tmpHiragana = {},
+							 	tmpKatakana = {};
 
-		for (vowel in vowels) {
-			if (vowels.hasOwnProperty(vowel)) {
-				var types = vowels[vowel];
-				for (type in types) {
-					if (types.hasOwnProperty(type)) {
-						var character = types[type],
-							tmp1 = character["Romaji"],
-						 	tmpHiragana = {},//character["Hiragana"],
-						 	tmpKatakana = {};
+							 tmpHiragana[character["Romaji"]] = character["Hiragana"];
+							 tmpKatakana[character["Romaji"]] = character["Katakana"];
 
-						 tmpHiragana[character["Romaji"]] = character["Hiragana"];
-						 tmpKatakana[character["Romaji"]] = character["Katakana"];
-
-						RomajiToHiragana[parent].push(tmpHiragana);
-						RomajiToKatakana[parent].push(tmpKatakana);
+							RomajiToHiragana[parent].push(tmpHiragana);
+							RomajiToKatakana[parent].push(tmpKatakana);
+						}
 					}
 				}
 			}
 		}
-	}
-} 
+	} 
+}
