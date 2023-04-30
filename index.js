79d04d5244e0b038c0c6a5b37f56226388259db2
Merwan Rodriguez <merwan@gmail.com>
2023-04-30 20:03:34 +0900

    renames kanji files to be more descriptive of their contents

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = require('./kana.json');
+module.exports = {
+  kana: require("./kana.json"),
+  kanji_map_n5: require("./kanji-map-n5.json"),
+  kanji_array_n5: require("./kanji-array-n5.json"),
+  kanji_array_n4: require("./kanji-array-n4.json"),
+  kanji_array_n3: require("./kanji-array-n3.json"),
+  kanji_array_n2: require("./kanji-array-n2.json"),
+  kanji_array_n1: require("./kanji-array-n1.json"),
+};
